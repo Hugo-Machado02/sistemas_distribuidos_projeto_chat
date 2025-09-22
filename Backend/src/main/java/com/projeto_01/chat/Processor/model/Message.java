1163456d9597954d3c8c41076c2d35b304f0fb76
Hugo Machado <hugo_machado02@hotmail.com>
2025-09-22 20:35:12 -0300

    ✨ feat: Commit Inicial do Projeto

---
+++ Backend/src/main/java/com/projeto_01/chat/Processor/model/Message.java
@@ -0,0 +1,23 @@
+package com.projeto_01.chat.Processor.model;
+
+public class Message {
+    private String name;
+    private String message;
+
+    public Message(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void saveMessage(String message) {
+        this.message = message;
+    }
+}
